1da487e0c40be62cc9527dd052d0173fa368e096
davepie101 <hyu028@ucr.edu>
2019-06-01 21:20:24 -0700

    finished basics of seat query

--- code/java/src/DBproject.java
+++ code/java/src/DBproject.java
@@ -451,13 +451,12 @@ public class DBproject{
 
 		try {
 			String query1 = String.format("SELECT P.seats - (SELECT F.num_sold FROM Plane P, Flight F, FlightInfo FL WHERE P.id = FL.plane_id AND FL.flight_id = F.fnum and F.fnum = %d AND F.actual_departure_date = '%s')", flight_num, depart_date); 
-			String query2 = String.format(" as Seats_Available FROM Plane P, Flight F, FlightInfo FL WHERE P.id = FL.plane_id AND FL.flight_id = F.fnum AND F.fnum = %d AND F.actual_departure_date = '%s'", flight_num, depart_date);
+			String query2 = String.format(" as Available_seats FROM Plane P, Flight F, FlightInfo FL WHERE P.id = FL.plane_id AND FL.flight_id = F.fnum AND F.fnum = %d AND F.actual_departure_date = '%s'", flight_num, depart_date);
 
 			query1 = query1 + query2;
+			System.out.print("\n");
 			esql.executeQueryAndPrintResult(query1);
-			int test = esql.executeQuery(query1);
-			System.out.print("Test: " + test);
-			System.out.print("The number of seats available for the following flight number and departure date is ");
+			System.out.print("\n");
 		}catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
